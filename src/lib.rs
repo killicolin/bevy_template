b40c3ba4a7eb023f29a00a457250924a764b022d
Killian Colin <killian.colin@proton.me>
2023-03-31 11:13:17 +0200

    [Update] bevy 0.9 to 0.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::{
     prelude::{default, App, PluginGroup},
-    window::{PresentMode, WindowDescriptor, WindowPlugin},
+    window::{PresentMode, Window, WindowPlugin, WindowResolution},
     DefaultPlugins,
 };
 use bevy_editor_pls::EditorPlugin;
@@ -8,13 +8,12 @@ use bevy_editor_pls::EditorPlugin;
 pub fn run(width: f32, height: f32) {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        window: WindowDescriptor {
-            title: "GAME NAME".to_string(),
-            width,
-            height,
+        primary_window: Some(Window {
+            title: "GAME_NAME".to_string(),
+            resolution: WindowResolution::new(width, height),
             present_mode: PresentMode::AutoVsync,
             ..default()
-        },
+        }),
         ..default()
     }));
     if cfg!(debug_assertions) {
